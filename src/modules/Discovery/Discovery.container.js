6cc5f321c1cd1a43c791a14dbbe789e99f0ad9fc
Khanh Tran-Cong <congkhanh197@gmail.com>
2019-06-16 15:27:12 +0700

    Add Discovery Screen

---
+++ src/modules/Discovery/Discovery.container.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+import DiscoveryScreen from './Discovery.screen'
+
+export default class DiscoveryContainer extends Component {
+    render() {
+        return (
+            <DiscoveryScreen />
+        )
+    }
+}
