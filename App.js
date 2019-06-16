90763aebda9f3e17d754a80edd122767f0af9a85
Khanh Tran-Cong <congkhanh197@gmail.com>
2019-06-16 18:54:19 +0700

    Add React Navigation config

--- App.js
+++ App.js
@@ -1,25 +1,45 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React from 'react';
+import { createSwitchNavigator, createStackNavigator, createAppContainer } from 'react-navigation';
 
-import React, { Component } from 'react';
-import { View } from 'react-native';
 import WelcomeContainer from './src/modules/Welcome/Welcome.container';
 import RegisterContainer from './src/modules/Register/Register.container';
 import VerifyContainer from './src/modules/Verify/Verify.container'
+import NameContainer from './src/modules/Name/Name.container'
+import GenderContainer from './src/modules/Gender/Gender.container'
+import AvatarContainer from './src/modules/Avatar/Avatar.container'
+import NotificationContainer from './src/modules/Notification/Notification.container'
+import MapContainer from './src/modules/Map/Map.container'
+import DiscoveryContainer from './src/modules/Discovery/Discovery.container'
+import AuthLoadingScreen from './src/navigation/AuthLoading.screen'
 
-
-
-export default class App extends Component {
-  render() {
-    return (
-        <View style={{flex:1}}>
-            <VerifyContainer/>
-        </View>
-    );
+const AppStack = createStackNavigator({
+  Maps: MapContainer,
+  Discovery: DiscoveryContainer
+}, {
+    defaultNavigationOptions: { header: null }
   }
-}
+);
+
+const AuthStack = createStackNavigator({
+  Welcome: WelcomeContainer,
+  Register: RegisterContainer,
+  Verify: VerifyContainer,
+  NameInput: NameContainer,
+  ChooseGender: GenderContainer,
+  Avatar: AvatarContainer,
+  NotifiPermision: NotificationContainer
+}, {
+    defaultNavigationOptions: { header: null }
+  }
+);
+
+export default createAppContainer(createSwitchNavigator(
+  {
+    AuthLoading: AuthLoadingScreen,
+    App: AppStack,
+    Auth: AuthStack,
+  },
+  {
+    initialRouteName: 'AuthLoading',
+  }
+));
