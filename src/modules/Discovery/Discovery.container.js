dead55a46b9dcc3eee825f374e549622381c67ac
Khanh Tran-Cong <congkhanh197@gmail.com>
2019-06-17 12:52:28 +0700

    Add navigation to the Discovery screen

--- src/modules/Discovery/Discovery.container.js
+++ src/modules/Discovery/Discovery.container.js
@@ -5,8 +5,11 @@ import DiscoveryScreen from './Discovery.screen'
 
 export default class DiscoveryContainer extends Component {
     render() {
+        data = {
+            onClickBack:()=>{this.props.navigation.goBack()}
+        }
         return (
-            <DiscoveryScreen />
+            <DiscoveryScreen {...data}/>
         )
     }
 }
