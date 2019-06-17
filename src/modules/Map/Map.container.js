abb8a10deb473f007c3b7073f1c4d36a61ddf050
Khanh Tran-Cong <congkhanh197@gmail.com>
2019-06-17 12:43:17 +0700

    Add navigation to the map screen

--- src/modules/Map/Map.container.js
+++ src/modules/Map/Map.container.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import AsyncStorage from '@react-native-community/async-storage';
 
 import MapScreen from './Map.screen'
 
 export default class MapContainer extends Component {
     render() {
+
+        data = {
+            onDiscovery : ()=>{this.props.navigation.navigate('Discovery')},
+            onPressProfile: async () => {
+                try {
+                  await AsyncStorage.removeItem('userToken')
+                } catch(e) {
+                  console.log('Error remove UserToken', e)
+                }
+                console.log('Done.')
+                this.props.navigation.navigate('AuthLoading')
+            }
+
+            
+        }
         return (
-            <MapScreen />
+            <MapScreen {...data}/>
         )
     }
 }
